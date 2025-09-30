19d1384b6a6099a1a63d3f1f98bc1f46b53b57ac
rndm13 <rndm13@users.noreply.github.com>
2025-09-30 21:45:24 +0300

    feat: add jack midi interface support

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include "hello_imgui/hello_imgui.h"
 #include "imgui-knobs/imgui-knobs.h"
 
+#include <jack/midiport.h>
+#include <jack/jack.h>
+
 #include "stdio.h"
 #include "stdint.h"
 
@@ -14,6 +17,10 @@
 
 using HelloImGui::Log;
 using HelloImGui::LogLevel;
+
+#define JACK_CLIENT_NAME "MicroKORG XL Controller"
+#define JACK_PORT_BUFFER_SIZE 4096
+#define DATA_QUEUE_SIZE 4096
 
 #define CONTROL_CHANGE_ENUM(VARIANT)                                 \
     VARIANT(CC_FILTER1_CUTOFF, 74)                                   \
@@ -35,7 +42,10 @@ const char* control_change_name(ControlChange cc) {
 }
 
 struct MidiInterface {
+    // TODO: Allow uint16_t values
+    // TODO: Specify channels (needed for specifying timbres)
     virtual bool send_control_change(uint8_t param_id, uint8_t val) = 0;
+    virtual bool initialize() = 0;
 };
 
 struct DummyMidi : public MidiInterface {
@@ -45,6 +55,96 @@ struct DummyMidi : public MidiInterface {
 
         Log(LogLevel::Debug, "CC %d %d", param_id, val);
         return true;
+    }
+
+    virtual bool initialize() {
+        Log(LogLevel::Debug, "Dummy MIDI controller successfully initialized");
+        return true;
+    }
+};
+
+struct JACKMidi : public MidiInterface {
+    jack_client_t* jack;
+    jack_port_t* jack_port;
+    jack_midi_data_t data_queue[DATA_QUEUE_SIZE];
+    size_t data_queue_size;
+
+    static int process(jack_nframes_t nframes, void* arg) {
+        assert(arg != nullptr);
+        JACKMidi *jack_midi = reinterpret_cast<JACKMidi*>(arg);
+        if (jack_midi->data_queue_size <= 0) {
+            return 0;
+        }
+
+        void* buf = jack_port_get_buffer(jack_midi->jack_port, nframes);
+        if (buf == NULL) {
+            Log(LogLevel::Error, "Failed to get port buffer\n");
+            return 1;
+        }
+
+        jack_midi_clear_buffer(buf);
+
+        int result = jack_midi_event_write(buf, nframes, jack_midi->data_queue, jack_midi->data_queue_size);
+        if (result != 0) {
+            Log(LogLevel::Error, "Failed to write MIDI event\n");
+            return 1;
+        }
+
+        memset(jack_midi->data_queue, 0, sizeof(jack_midi->data_queue));
+        jack_midi->data_queue_size = 0;
+        return 0;
+    }
+
+    virtual bool send_control_change(uint8_t param_id, uint8_t val) {
+        assert(param_id < 128);
+        assert(val < 128);
+
+        this->data_queue[this->data_queue_size++] = 0xB0;
+        this->data_queue[this->data_queue_size++] = param_id;
+        this->data_queue[this->data_queue_size++] = val;
+        
+        return true;
+    }
+
+    virtual bool initialize() {
+        memset(this->data_queue, 0, sizeof(this->data_queue));
+        this->data_queue_size = 0;
+        this->jack = jack_client_open(JACK_CLIENT_NAME, JackNoStartServer, NULL);
+
+        if (this->jack == NULL) {
+            Log(LogLevel::Error, "Failed to open JACK client\n");
+        }
+
+        Log(LogLevel::Info, "Successfully opened JACK client\n");
+
+        int err = jack_set_process_callback(this->jack, JACKMidi::process, this);
+        if (err != 0) {
+            Log(LogLevel::Error, "Failed to set JACK process callback. Err: %d\n", err);
+            goto err_client;
+        }
+
+        this->jack_port = jack_port_register(this->jack, "MIDI Out", JACK_DEFAULT_MIDI_TYPE, JackPortIsOutput, JACK_PORT_BUFFER_SIZE);
+        if (this->jack_port == NULL) {
+            Log(LogLevel::Error, "Failed to register JACK port\n");
+            goto err_client;
+        }
+
+        Log(LogLevel::Info, "Successfully registered JACK port\n");
+
+        err = jack_activate(this->jack);
+        if (err != 0) {
+            Log(LogLevel::Error, "Failed to activate jack. Err: %d\n", err);
+            goto err_port;
+        }
+
+        Log(LogLevel::Info, "JACK MIDI controller successfully initialized");
+        return true;
+err_port:
+        jack_port_unregister(this->jack, this->jack_port);
+err_client:
+        jack_client_close(this->jack);
+err:
+        return false;
     }
 };
 
@@ -68,7 +168,7 @@ bool app_init(App& out_app) {
     Log(LogLevel::Info, "Initializing controller");
 
     out_app.program = {};
-    out_app.midi = new DummyMidi();
+    out_app.midi = new JACKMidi();
 
     if (out_app.midi == nullptr) {
         Log(LogLevel::Error, "Failed to allocate MIDI interface");
@@ -76,7 +176,13 @@ bool app_init(App& out_app) {
         return false;
     }
 
-    Log(LogLevel::Error, "Successfully initialized Controller");
+    if (!out_app.midi->initialize()) {
+        Log(LogLevel::Error, "Failed to initialize MIDI interface");
+
+        return false;
+    }
+
+    Log(LogLevel::Error, "Successfully initialized controller");
 
     return true;
 }
@@ -112,7 +218,7 @@ void app_gui() {
 int main(int , char *[]) {
     bool succ = app_init(g_app);
     if (!succ) {
-        printf("Failed to initialize controller\n");
+        Log(LogLevel::Error, "Failed to initialize controller\n");
         return 1;
     }
 
