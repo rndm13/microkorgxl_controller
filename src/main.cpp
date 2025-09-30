05aab66a090719826282bcd8c38e756a0806fcec
rndm13 <rndm13@users.noreply.github.com>
2025-09-30 21:34:48 +0300

    feat: initial commit

---
+++ src/main.cpp
@@ -0,0 +1,135 @@
+#include "immapp/immapp.h"
+
+#ifdef IMGUI_BUNDLE_WITH_IMPLOT
+#include "implot/implot.h"
+#endif
+
+#include "hello_imgui/hello_imgui.h"
+#include "imgui-knobs/imgui-knobs.h"
+
+#include "stdio.h"
+#include "stdint.h"
+
+#include "assert.h"
+
+using HelloImGui::Log;
+using HelloImGui::LogLevel;
+
+#define CONTROL_CHANGE_ENUM(VARIANT)                                 \
+    VARIANT(CC_FILTER1_CUTOFF, 74)                                   \
+    VARIANT(CC_FILTER1_RESO, 71)                                     
+
+#define CONTROL_CHANGE_ENUM_VARIANT(NAME, VALUE) NAME = VALUE,
+#define CONTROL_CHANGE_ENUM_STRING(NAME, VALUE) case NAME: return #NAME;
+
+enum ControlChange {
+    CONTROL_CHANGE_ENUM(CONTROL_CHANGE_ENUM_VARIANT)
+};
+
+const char* control_change_name(ControlChange cc) {
+    switch (cc) {
+        CONTROL_CHANGE_ENUM(CONTROL_CHANGE_ENUM_STRING)
+        default: assert(!"Unknown Control Change");
+    }
+    return "";
+}
+
+struct MidiInterface {
+    virtual bool send_control_change(uint8_t param_id, uint8_t val) = 0;
+};
+
+struct DummyMidi : public MidiInterface {
+    virtual bool send_control_change(uint8_t param_id, uint8_t val) {
+        assert(param_id < 128);
+        assert(val < 128);
+
+        Log(LogLevel::Debug, "CC %d %d", param_id, val);
+        return true;
+    }
+};
+
+struct Filter {
+    int cutoff;
+    int resonance;
+};
+
+struct Program {
+    Filter filter_1;
+};
+
+struct Controller {
+    Program program;
+    MidiInterface* midi;
+};
+
+Controller g_cntr = {};
+
+bool controller_init(Controller& out_controller) {
+    Log(LogLevel::Info, "Initializing Controller");
+
+    out_controller.program = {};
+    out_controller.midi = new DummyMidi();
+
+    if (out_controller.midi == nullptr) {
+        Log(LogLevel::Error, "Failed to allocate MIDI interface");
+
+        return false;
+    }
+
+    Log(LogLevel::Error, "Successfully initialized Controller");
+
+    return true;
+}
+
+void parameter_knob(int& param, ControlChange cc) {
+    if (ImGuiKnobs::KnobInt(control_change_name(cc), &param, 0, 127, 1, "%d", ImGuiKnobVariant_Tick)) { \
+        g_cntr.midi->send_control_change(cc, param); \
+    }
+}
+
+void filter_gui(Filter& filter, const char* window_name) {
+    if (ImGui::Begin(window_name)) {
+        parameter_knob(filter.cutoff, CC_FILTER1_CUTOFF);
+        ImGui::SameLine();
+        parameter_knob(filter.resonance, CC_FILTER1_RESO);
+    }
+
+    ImGui::End(); // Begin
+}
+
+void program_gui(Program& program) {
+    filter_gui(program.filter_1, "Filter 1");
+}
+
+void gui() {
+    program_gui(g_cntr.program);
+
+#ifndef NDEBUG
+    ImGui::ShowDemoWindow();
+#endif
+}
+
+
+int main(int , char *[]) {
+    bool succ = controller_init(g_cntr);
+    if (!succ) {
+        printf("Failed to initialize controller\n");
+        return 1;
+    }
+
+
+#ifdef ASSETS_LOCATION
+    HelloImGui::SetAssetsFolder(ASSETS_LOCATION);
+#endif
+
+    HelloImGui::SimpleRunnerParams runnnerParams;
+    runnnerParams.guiFunction = gui;
+    runnnerParams.windowSize = {600, 800};
+
+    ImmApp::AddOnsParams addOnsParams;
+    addOnsParams.withMarkdown = true;
+    addOnsParams.withImplot = true;
+
+    ImmApp::Run(runnnerParams, addOnsParams);
+    return 0;
+}
